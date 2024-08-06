36cd151a5200a3d95a1bd8140bd556bef7f2cc7a
KantaHasegawa-26 <161685065+26KantaHasegawa@users.noreply.github.com>
2024-08-06 19:20:25 +0900

    distを含める

---
+++ packages/svelte/dist/lib/plugins/Controls/ControlButton.svelte.d.ts
@@ -0,0 +1,21 @@
+import { SvelteComponentTyped } from "svelte";
+import type { ControlButtonProps } from './types';
+declare const __propDef: {
+    props: ControlButtonProps;
+    events: {
+        click: MouseEvent;
+    } & {
+        [evt: string]: CustomEvent<any>;
+    };
+    slots: {
+        default: {
+            class: string;
+        };
+    };
+};
+type ControlButtonProps_ = typeof __propDef.props;
+export { ControlButtonProps_ as ControlButtonProps };
+export type ControlButtonEvents = typeof __propDef.events;
+export type ControlButtonSlots = typeof __propDef.slots;
+export default class ControlButton extends SvelteComponentTyped<ControlButtonProps_, ControlButtonEvents, ControlButtonSlots> {
+}
